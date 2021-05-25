6de48f9c31b6a64dec384fe1dfdd95ea132f6516
Sanmir Cruz <sanmir.cruz@gmail.com>
2021-05-25 13:44:08 -0300

    Alterando conexão com o banco de dados para o 'remotemysql.com'

--- assets/php/conexao.php
+++ assets/php/conexao.php
@@ -4,10 +4,15 @@
 	// $password = "ACkbFOas1TF3iKz";
 	// $dbname = "epiz_28647293_db_teste_infinity";
     
-	$server = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "db_progresso_tcc";
+	// $server = "localhost";
+	// $username = "root";
+	// $password = "";
+	// $dbname = "db_progresso_tcc";
+    
+	$server = "remotemysql.com";
+	$username = "3bgipIFF8X";
+	$password = "xl1plSdRp6";
+	$dbname = "3bgipIFF8X";
 
 	$con = new mysqli($server, $username, $password, $dbname);
 ?>
